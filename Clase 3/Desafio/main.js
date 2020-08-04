82e17750f1ade663c5b0d3ea4585afafea831d5c
g_kogan@yahoo.com.ar <64507135+guidokogan@users.noreply.github.com>
2020-08-04 15:01:36 -0300

    Curso Javascript - Desafio 3

--- Clase 3/Desafio/main.js
+++ Clase 3/Desafio/main.js
@@ -2,16 +2,8 @@ var marca = ["Volkswagen", "Renault", "Ford", "Peugeot", "Chevrolet", "Fiat", "C
 
 function listar_marcas() {
     for (var n=0; n < marca.length; n++){
-        if(n == 1){
-            console.log(marca[n] + " " + n)
-            continue
-        }
-        if(n == 3){
-            console.log(marca[n] + " " + n)
-            continue
-        }
-        if(n == 5){
-            console.log(marca[n] + " " + n)
+        if (n % 2 === 0) {
+            console.log(marca[n] + " " + n);
             continue
         }
         if(n == 7){
