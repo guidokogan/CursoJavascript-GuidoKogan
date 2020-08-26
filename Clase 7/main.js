e5fc2390e08644071b03b15ee37a1c0a53a75be8
g_kogan@yahoo.com.ar <64507135+guidokogan@users.noreply.github.com>
2020-08-26 19:40:59 -0300

    Curso Javascript Clase 8

--- Clase 7/main.js
+++ Clase 7/main.js
@@ -4,11 +4,11 @@ function guardar(){
     datosJSON.nombre = document.getElementById('nombre').value;
     datosJSON.socio = document.getElementById('socio').value;
     datosJSON.email = document.getElementById('email').value;
-    localStorage.setItem('datosformulario', btoa(JSON.stringify(datosJSON)))
+    localStorage.setItem('datosformulario', atob(JSON.stringify(datosJSON)))
 }
 
 function recuperar(){
-    datosJSON = JSON.parse(atob(localStorage.getItem('datosformulario')))
+    datosJSON = JSON.parse(btoa(localStorage.getItem('datosformulario')))
     document.getElementById('nombre').value = datosJSON.nombre;
     document.getElementById('socio').value = datosJSON.socio;
     document.getElementById('email').value = datosJSON.email;
--- EntregaFinal/main.js
+++ EntregaFinal/main.js
@@ -1,73 +1,129 @@
-var carOptions = {
+function getYearValue(yearSelected) {
+  var yearSelected = formInfo__options__year.value;
+  switch (true) {
+    case yearSelected < 1950:
+      return 28;
+      break;
+    case yearSelected < 1970:
+      return 26;
+      break;
+    case yearSelected < 1990:
+      return 24;
+      break;
+    case yearSelected < 2000:
+      return 22;
+      break;
+    case yearSelected < 2005:
+      return 20;
+      break;
+    case yearSelected < 2010:
+      return 18;
+      break;
+    case yearSelected <= 2015:
+      return 16;
+      break;
+    case yearSelected > 2015:
+      return 14;
+      break;
+  }
+}
+
+
+var car = document.getElementById("formInfo__options__marca");
+var model = document.getElementById("formInfo__options__model");
+
+var carSelected = car.options[car.selectedIndex].value;
+var modelSelected = model.options[model.selectedIndex].value;
+var marca = carSelected;
+var modelo = modelSelected;
+
+function getModelValue(carSelected, modelSelected) {
+  
+  var carOptions = {
     volkswagen: {
-      sedan: 0.2,
-      suv: 0.32,
-      golf: 0.35,
+      sedan: 32,
+      suv: 33,
+      golf: 35,
     },
     renault: {
-      onix: 0.14,
-      cruze: 0.18,
-      joy: 0.22,
+      onix: 34,
+      cruze: 38,
+      joy: 32,
     },
     ford: {
-      ka: 0.5,
-      mondeo: 0.6,
-      mustang: 1.2,
+      ka: 35,
+      mondeo: 36,
+      mustang: 32,
     },
   };
-  
-  function getModelValue(brandInput, modelInput) {
-    // renault joy
-    var brand = brandInput.toLowerCase();
-    var model = modelInput.toLowerCase();
-    if (brand in carOptions) {
-      if (model in carOptions[brand]) {
-        return carOptions[brand][model];
-      } else {
-        console.log("no encuentro el modelo");
-      }
-    } else {
-      console.log("no encuentro la marca");
-    }
-  }
-  
-  function getYearValue(year) {
-    switch (true) {
-      case year < 1950:
-        return 2.5;
-        break;
-      case year < 1970:
-        return 2.3;
-        break;
-      case year < 1990:
-        return 2;
-        break;
-      case year < 2000:
-        return 1.8;
-        break;
-      case year < 2005:
-        return 1.6;
-        break;
-      case year < 2010:
-        return 1.4;
-        break;
-      case year <= 2015:
-        return 1.2;
-        break;
-      case year > 2015:
-        return 0.8;
-        break;
-    }
-  }
-  
-  function getPrice(brand, model, year, price) {
-    var totalData = new Object();
-    totalData.price = price;
-    totalData.modelValue = getModelValue(brand, model);
-    totalData.yearValue = getYearValue(year);
-    totalData.total = price * totalData.modelValue * totalData.yearValue;
-    return "El valor del seguro es: " + totalData.total;
-  }
-  
 
-var fordMustang = getPrice("ford", "mustang", 1920, 800000);
+  if (marca in carOptions) {
+    if (modelo in carOptions[marca]) {
+      return carOptions[marca][modelo];
+    } else {
+      console.log("no encuentro el modelo");
+    }
+  } else {
+    console.log("no encuentro la marca");
+  }
+}
+
+
+var onix = document.getElementById("onix");
+var cruze = document.getElementById("cruze");
+var joy = document.getElementById("joy");
+var ka = document.getElementById("ka");
+var mondeo = document.getElementById("mondeo");
+var mustang = document.getElementById("mustang");
+var sedan = document.getElementById("sedan");
+var suv = document.getElementById("suv");
+var golf = document.getElementById("golf");
+
+function removeExtraSelections() {
+  if (carSelected == "volkswagen") {
+    onix.parentNode.removeChild(onix);
+    cruze.parentNode.removeChild(cruze);
+    joy.parentNode.removeChild(joy);
+    ka.parentNode.removeChild(ka);
+    mondeo.parentNode.removeChild(mondeo);
+    mustang.parentNode.removeChild(mustang);
+    } else if (carSelected == "renault") {
+      sedan.parentNode.removeChild(sedan);
+      suv.parentNode.removeChild(suv);
+      golf.parentNode.removeChild(golf);
+      ka.parentNode.removeChild(ka);
+      mondeo.parentNode.removeChild(mondeo);
+      mustang.parentNode.removeChild(mustang);
+    } else if (carSelected == "ford") {
+      sedan.parentNode.removeChild(sedan);
+      suv.parentNode.removeChild(suv);
+      golf.parentNode.removeChild(golf);
+      onix.parentNode.removeChild(onix);
+      cruze.parentNode.removeChild(cruze);
+      joy.parentNode.removeChild(joy);
+    } else {
+      sedan.parentNode.removeChild(sedan);
+      suv.parentNode.removeChild(suv);
+      golf.parentNode.removeChild(golf);
+      onix.parentNode.removeChild(onix);
+      cruze.parentNode.removeChild(cruze);
+      joy.parentNode.removeChild(joy);
+      ka.parentNode.removeChild(ka);
+      mondeo.parentNode.removeChild(mondeo);
+      mustang.parentNode.removeChild(mustang);
+    } 
+}
+
+
+function getPrice(carSelected, modelSelected, yearSelected, valueSelected) {
+  var valueSelected = formInfo__valor.value;
+  var totalData = new Object();
+  totalData.valueSelected = valueSelected;
+  totalData.modelValue = getModelValue(carSelected, modelSelected);
+  totalData.yearValue = getYearValue(yearSelected);
+  totalData.total = totalData.valueSelected / totalData.modelValue / totalData.yearValue;
+  totalData.total = +totalData.total.toFixed(2);
+  document.getElementById('getPrice').innerHTML = "$" + totalData.total;
+  removeExtraSelections();
+}
+
