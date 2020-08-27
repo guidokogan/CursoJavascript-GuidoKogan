935dc500ac35615bf490bc849c2f19043fb9019e
g_kogan@yahoo.com.ar <64507135+guidokogan@users.noreply.github.com>
2020-08-26 23:10:11 -0300

    Curso Javascript Clase 9

--- EntregaFinal/main.js
+++ EntregaFinal/main.js
@@ -1,120 +1,108 @@
+// Fijarse si se puede hacer todo el codigo más corto
+// La función function populate(s1, s2) la saque de internet y no se porqué cuando cambio el "s1" y el "s2", no toma nada.
+// No se si entendí que significa esto, osea, sí, pero porqué split y porque optionArray "var pair = optionArray[option].split("|");"
+// También estaría bueno que pueda agarrar el contenido del objeto definido de los nombres de las marcas de autos en vez de volver a escribirlas.
+
+
+formInfo__options__year.addEventListener("focus", function () {
+  this.placeholder = "";
+});
+formInfo__options__year.addEventListener("blur", function () {
+  this.placeholder = "Ej. Ford";
+});
+formInfo__valor.addEventListener("focus", function () {
+  this.placeholder = "";
+});
+formInfo__valor.addEventListener("blur", function () {
+  this.placeholder = "Ej. 100.000";
+});
+
 function getYearValue(yearSelected) {
   var yearSelected = formInfo__options__year.value;
   switch (true) {
-    case yearSelected < 1950:
-      return 28;
-      break;
-    case yearSelected < 1970:
-      return 26;
-      break;
-    case yearSelected < 1990:
-      return 24;
-      break;
-    case yearSelected < 2000:
-      return 22;
-      break;
-    case yearSelected < 2005:
-      return 20;
-      break;
-    case yearSelected < 2010:
-      return 18;
-      break;
-    case yearSelected <= 2015:
-      return 16;
-      break;
-    case yearSelected > 2015:
-      return 14;
-      break;
+      case yearSelected < 1950:
+          return 28;
+          break;
+      case yearSelected < 1970:
+          return 26;
+          break;
+      case yearSelected < 1990:
+          return 24;
+          break;
+      case yearSelected < 2000:
+          return 22;
+          break;
+      case yearSelected < 2005:
+          return 20;
+          break;
+      case yearSelected < 2010:
+          return 18;
+          break;
+      case yearSelected <= 2015:
+          return 16;
+          break;
+      case yearSelected > 2015:
+          return 14;
+          break;
   }
 }
-
 
 var car = document.getElementById("formInfo__options__marca");
 var model = document.getElementById("formInfo__options__model");
 
-var carSelected = car.options[car.selectedIndex].value;
-var modelSelected = model.options[model.selectedIndex].value;
-var marca = carSelected;
-var modelo = modelSelected;
-
-function getModelValue(carSelected, modelSelected) {
-  
-  var carOptions = {
-    volkswagen: {
-      sedan: 32,
-      suv: 33,
-      golf: 35,
-    },
-    renault: {
-      onix: 34,
-      cruze: 38,
-      joy: 32,
-    },
-    ford: {
-      ka: 35,
-      mondeo: 36,
-      mustang: 32,
-    },
-  };
-
-  if (marca in carOptions) {
-    if (modelo in carOptions[marca]) {
-      return carOptions[marca][modelo];
-    } else {
-      console.log("no encuentro el modelo");
-    }
-  } else {
-    console.log("no encuentro la marca");
+function populate(s1, s2) {
+  var s1 = car;
+  var s2 = model;
+  s2.innerHTML = "";
+  if (s1.value == "volkswagen") {
+      var optionArray = ["|", "sedan|Sedan", "suv|Suv", "golf|Golf"];
+  } else if (s1.value == "renault") {
+      var optionArray = ["|", "onix|Onix", "cruze|Cruze", "joy|Joy"];
+  } else if (s1.value == "ford") {
+      var optionArray = ["|", "ka|Ka", "mondeo|Mondeo", "mustang|Mustang"];
+  }
+  for (var option in optionArray) {
+      var pair = optionArray[option].split("|");
+      var newOption = document.createElement("option");
+      newOption.value = pair[0];
+      newOption.innerHTML = pair[1];
+      s2.options.add(newOption);
   }
 }
 
+function getModelValue(carSelected, modelSelected) {
+  var carSelected = car.options[car.selectedIndex].value;
+  var modelSelected = model.options[model.selectedIndex].value;
+  var marca = carSelected;
+  var modelo = modelSelected;
+  var carOptions = {
+      volkswagen: {
+          sedan: 32,
+          suv: 33,
+          golf: 35,
+      },
+      renault: {
+          onix: 34,
+          cruze: 38,
+          joy: 32,
+      },
+      ford: {
+          ka: 35,
+          mondeo: 36,
+          mustang: 32,
+      },
+  };
 
-var onix = document.getElementById("onix");
-var cruze = document.getElementById("cruze");
-var joy = document.getElementById("joy");
-var ka = document.getElementById("ka");
-var mondeo = document.getElementById("mondeo");
-var mustang = document.getElementById("mustang");
-var sedan = document.getElementById("sedan");
-var suv = document.getElementById("suv");
-var golf = document.getElementById("golf");
-
-function removeExtraSelections() {
-  if (carSelected == "volkswagen") {
-    onix.parentNode.removeChild(onix);
-    cruze.parentNode.removeChild(cruze);
-    joy.parentNode.removeChild(joy);
-    ka.parentNode.removeChild(ka);
-    mondeo.parentNode.removeChild(mondeo);
-    mustang.parentNode.removeChild(mustang);
-    } else if (carSelected == "renault") {
-      sedan.parentNode.removeChild(sedan);
-      suv.parentNode.removeChild(suv);
-      golf.parentNode.removeChild(golf);
-      ka.parentNode.removeChild(ka);
-      mondeo.parentNode.removeChild(mondeo);
-      mustang.parentNode.removeChild(mustang);
-    } else if (carSelected == "ford") {
-      sedan.parentNode.removeChild(sedan);
-      suv.parentNode.removeChild(suv);
-      golf.parentNode.removeChild(golf);
-      onix.parentNode.removeChild(onix);
-      cruze.parentNode.removeChild(cruze);
-      joy.parentNode.removeChild(joy);
-    } else {
-      sedan.parentNode.removeChild(sedan);
-      suv.parentNode.removeChild(suv);
-      golf.parentNode.removeChild(golf);
-      onix.parentNode.removeChild(onix);
-      cruze.parentNode.removeChild(cruze);
-      joy.parentNode.removeChild(joy);
-      ka.parentNode.removeChild(ka);
-      mondeo.parentNode.removeChild(mondeo);
-      mustang.parentNode.removeChild(mustang);
-    } 
+  if (marca in carOptions) {
+      if (modelo in carOptions[marca]) {
+          return carOptions[marca][modelo];
+      } else {
+          console.log("no encuentro el modelo");
+      }
+  } else {
+      console.log("no encuentro la marca");
+  }
 }
-
-
 function getPrice(carSelected, modelSelected, yearSelected, valueSelected) {
   var valueSelected = formInfo__valor.value;
   var totalData = new Object();
@@ -123,7 +111,24 @@ function getPrice(carSelected, modelSelected, yearSelected, valueSelected) {
   totalData.yearValue = getYearValue(yearSelected);
   totalData.total = totalData.valueSelected / totalData.modelValue / totalData.yearValue;
   totalData.total = +totalData.total.toFixed(2);
-  document.getElementById('getPrice').innerHTML = "$" + totalData.total;
-  removeExtraSelections();
+  document.getElementById("getPrice").innerHTML = "$" + totalData.total;
 }
 
+
+function mouseDown() {
+  document.getElementById("saveCotizacion").style.backgroundColor="green";
+}
+function mouseUp() {
+  document.getElementById("saveCotizacion").style.backgroundColor = "red";
+}
+
+var datosJSON = {"yearSelected": null, "valueSelected": null, "carSelected": "", "modelSelected": ""}
+
+function saveCotizacion() {
+  datosJSON.yearSelected = formInfo__options__year.value;
+  datosJSON.valueSelected = formInfo__valor.value;
+  datosJSON.carSelected = car.options[car.selectedIndex].value;
+  datosJSON.modelSelected = model.options[model.selectedIndex].value;
+  localStorage.setItem('datosformulario', (JSON.stringify(datosJSON)));
+  document.getElementById("saveCotizacion").style.backgroundColor = "#121c42";
+}
