88c5417276f9d6891661f72176d63651fcf68930
g_kogan@yahoo.com.ar <64507135+guidokogan@users.noreply.github.com>
2020-09-09 22:43:55 -0300

    Curso Javascript Clase 10

--- Clase 1/Desafio/main.js
+++
@@ -1,7 +0,0 @@
-var nombre = prompt("Ingrese su primer nombre", "Guido");
-var apellido = prompt("Ingrese su apellido", "Kogan");
-
-if (nombre != null) {
-    alert("Hola " + nombre + " " + apellido + "! Como has estado?");
-    document.getElementById("nombre").innerHTML = "Hola " + nombre + " " + apellido + "! Como has estado?";
-}
